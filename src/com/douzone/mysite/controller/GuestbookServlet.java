3b046bad17f8d5d5b2b1e778a8369b5fee16af10
kickscar <kickscar@192.168.0.98>
2019-01-24 16:10:55 +0900

    guestbook 완

---
+++ src/com/douzone/mysite/controller/GuestbookServlet.java
@@ -0,0 +1,33 @@
+package com.douzone.mysite.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.douzone.mvc.action.AbstractActionFactory;
+import com.douzone.mvc.action.Action;
+import com.douzone.mysite.action.guestbook.GuestActionFactory;
+
+@WebServlet("/guestbook")
+public class GuestbookServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String actionName = request.getParameter("a");
+
+		AbstractActionFactory af = new GuestActionFactory();
+		Action action = af.getAction(actionName);
+		action.execute(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
